b88225a43f997c31b20b44c698ec80e1b98423b6
agentreno <kturrell@hotmail.com>
2015-11-19 21:07:37 +0000

    Partials route, bower package and angular dependency

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
+from jinja2.exceptions import TemplateNotFound
 from app import app
 
 @app.route("/")
 def index():
    return render_template("home.html", title="Hello")
+
+@app.route("/partials/<partialname>")
+def getpartial(partialname):
+   try:
+      return render_template("partials/" + partialname)
+   except TemplateNotFound:
+      return "", 404
